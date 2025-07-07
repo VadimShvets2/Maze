b6b60fc6450087d2d1bd45c5a1730ca75eddcd62
Vadim Shvetsov <vadimshvetsov619@gmail.com>
2025-07-07 11:05:26 -0500

    Maze

---
+++ Assets/Script/Controller.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    [SerializeField] private float Speed;
+    [SerializeField] private float Horizontal_Sensitive = 1;
+    [SerializeField] private float Verticles_Sensitive = 1;
+    private Rigidbody Human;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Human = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Move();
+       Rotate();
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Human.AddForce(transform.forward * Speed);  
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Human.AddForce(-transform.right * Speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Human.AddForce(transform.right * Speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Human.AddForce(-transform.forward * Speed); 
+        }
+    }
+
+    void Rotate()
+    {
+        Vector3 Rotation = transform.eulerAngles;
+        float Horizontal = Input.GetAxis("Mouse X") * Horizontal_Sensitive;
+        float Vertical = Input.GetAxis("Mouse Y") * Verticles_Sensitive;
+        Rotation.x += Vertical * -1;
+        Rotation.y += Horizontal;
+        transform.eulerAngles = Rotation;
+    }
+}
